2a40f7dc68cac748de385ef8ab2b38781b1690b1
shubhamhajare27 <shubhamhajare27@shubhamhajare4@gmail.com>
2019-12-04 22:22:28 +0530

    first commit

---
+++ tictactoe-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '/home/shubham/Desktop/React/React First Tutorial/node_modules/bootstrap/dist/css/bootstrap.css';
+import App from './App';
+import Box from './components/box';
+import './index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
