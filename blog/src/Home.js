5c5ab34eae1f03c86b9288a0e962ac2b25d63ba1
shubhamhajare27 <52093961+shubhamhajare27@users.noreply.github.com>
2021-07-10 07:50:58 +0530

    Loading error messages

--- blog/src/Home.js
+++ blog/src/Home.js
@@ -4,22 +4,37 @@ const Home = () => {
 
 const [blogs,setblogs]=useState(null);
 const [isPending,setIsPending]=useState(true);
+const [error,setError]=useState(null);
 
 useEffect(()=>{
   fetch('http://localhost:8000/blogs')
   .then(res =>{
-    res.json()
-      .then(data =>{
-        console.log(data)
-        setblogs(data);
-        setIsPending(false);
+    if(res.ok)
+    {
+      res.json()
+       .then(data =>{
+         console.log(data)
+         setError(null);
+
+         setblogs(data);
+         setIsPending(false);
       })
+    }
+    else{
+      throw Error('failed');
+    }
+  })
+  .catch(err=>{
+    setError(err.message);
+
+    setIsPending(false);
   })
   
 },[ ]);
 
     return ( 
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <h2>Loading...</h2>}
       {blogs && < BlogList blogs={blogs} title="All Blogs" />}
     </div> );
