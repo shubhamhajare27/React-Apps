86c11cf1ee8110520773fd0c8861f563b547d3e2
shubhamhajare27 <52093961+shubhamhajare27@users.noreply.github.com>
2021-07-10 12:49:03 +0530

    submit event

--- blog/src/blogsList.js
+++ blog/src/blogsList.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 const BlogList = ({blogs ,title })=>{
     // const blogs=props.blogs;
     // const title=props.title;
@@ -9,8 +10,10 @@ const BlogList = ({blogs ,title })=>{
                 <h2>{title}</h2>
                 {blogs.map((blog)=>(
                     <div className="blog-preview" key={blog.id}>
-                        <h2>{blog.title}</h2>
-                        <p>Written By {blog.author}</p>
+                        <Link to={`blogs/${blog.id}`}>
+                            <h2>{blog.title}</h2>
+                            <p>Written By {blog.author}</p>
+                        </Link>
                         {/* <button onClick={()=> handleDelete(blog.id)}>Delete Blog</button> */}
                     </div>
                 ))}
